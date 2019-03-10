13e5fb70b37c9a25cad286c6a14f15640f2ccd68
Luis Lau <theauthorgm@gmail.com>
2019-03-10 11:01:47 -0500

    WIP setup electron

--- electron.js
+++ electron.js
@@ -1,6 +1,6 @@
 const {app, BrowserWindow} = require('electron');
 
 app.on('ready', () => {
-  const window = new BrowserWindow();
+  const window = new BrowserWindow({});
   window.loadFile('index.html');
 });
